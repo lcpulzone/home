34f716f93fbbd2aa2051490dcc6047cb5092ff06
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-02-17 09:53:12 -0800

    Build minitests to test methods

--- test/apartment_test.rb
+++ test/apartment_test.rb
@@ -4,4 +4,32 @@ require './lib/apartment'
 require './lib/renter'
 
 class ApartmentTest < Minitest::Test
+
+  def test_it_exists
+    # skip
+    apartment = Apartment.new({number: "", monthly_rent: 0, bathrooms: 0, bedrooms: 0})
+    assert_instance_of Apartment, apartment
+  end
+
+  def test_it_has_a_renter
+    # skip
+    apartment = Apartment.new({number: "", monthly_rent: 0, bathrooms: 0, bedrooms: 1})
+    renter1 = Renter.new("Jessie")
+    assert_equal "Jessie", renter1.name
+  end
+
+  def test_it_has_a_unit
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    assert_equal "A1", unit1.number
+    assert_equal 1200, unit1.monthly_rent
+    assert_equal 1, unit1.bathrooms
+    assert_equal 1, unit1.bedrooms
+  end
+
+  def test_it_can_add_a_renter
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    renter1 = Renter.new("Jessie")
+    assert_equal [renter1], unit1.add_renter(renter1)
+    assert_equal [renter1], unit1.renters
+  end
 end
--- test/building_test.rb
+++ test/building_test.rb
@@ -5,4 +5,50 @@ require './lib/apartment'
 require './lib/renter'
 
 class BuildingTest < Minitest::Test
+  def test_it_exists
+    building = Building.new
+  assert_instance_of Building, building
+  end
+
+  def test_it_has_an_empty_array
+    building = Building.new
+    assert_equal [], building.units
+  end
+
+  def test_it_can_add_units
+    building = Building.new
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+    assert_equal [unit1], building.add_unit(unit1)
+    assert_equal [unit1, unit2], building.add_unit(unit2)
+  end
+
+  def test_it_has_units
+    building = Building.new
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+    building.add_unit(unit1)
+    building.add_unit(unit2)
+    assert_equal [unit1, unit2], building.units
+  end
+
+  def test_it_can_add_a_renter
+    skip
+    building = Building.new
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    renter1 = Renter.new("Aurora")
+    assert_equal [], building.renters
+    building.add_renter(renter1)
+    assert_equal ["Aurora"], building.renters
+  end
+
+  def test_it_can_add_renters
+  building = Building.new
+  unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+  renter1 = Renter.new("Aurora")
+  renter2 = Renter.new("Tim")
+  building.add_renter(renter1)
+  building.add_renter(renter2)
+  assert_equal ["Aurora", "Tim"], building.renters
+  end
 end
